9c6bf7ce9ed5ccffcb6f6a08cff6e0549ed59dc0
Vincent Kelleher <Vincent@MacBook-Pro-de-Vincent.local>
2014-12-09 17:39:50 +0100

    Convolution class with red tests

--- src/convolution.js
+++ src/convolution.js
@@ -1,3 +1,22 @@
 /**
  * Convolution main class
  */
+
+var Convolution = function() {
+    this.inputMatrix = [];
+    this.convolutionMatrix = [];
+};
+
+Convolution.prototype.setInputMatrix = function(inputMatrix) {
+    this.inputMatrix = inputMatrix;
+};
+
+Convolution.prototype.setConvolutionMatrix = function(convolutionMatrix) {
+    this.convolutionMatrix = convolutionMatrix;
+};
+
+Convolution.prototype.compute = function() {
+    
+
+    return [];
+};
---
+++ test/test_convolution.js
@@ -0,0 +1,40 @@
+/**
+ * Convolution test class
+ */
+
+describe("Convolution network", function() {
+    beforeEach(function() {
+        this.convolution = new Convolution();
+    });
+
+    it("processes a matrix", function() {
+        var inputMatrix = [
+            [ 2, 1, 0, 2, 0, 1 ],
+            [ 0, 1, 2, 2, 1, 1 ],
+            [ 0, 3, 0, 0, 0, 2 ],
+            [ 1, 1, 1, 0, 2, 2 ],
+            [ 0, 0, 3, 1, 1, 2 ],
+            [ 0, 1, 0, 0, 2, 0 ]
+        ];
+
+        var convolutionMatrix = [
+            [ 1, 0, 1 ],
+            [ 0, 2, 0 ],
+            [ 1, 0, 1 ]
+        ];
+
+        var expectedOutputMatrix = [
+            [ 4 , 10, 4, 7 ],
+            [ 10, 4 , 6, 5 ],
+            [ 5 , 6 , 4, 9 ],
+            [ 2 , 8 , 7, 4 ]
+        ];
+
+        this.convolution.setInputMatrix(inputMatrix);
+        this.convolution.setConvolutionMatrix(convolutionMatrix);
+
+        var outputMatrix = this.convolution.compute();
+
+        expect(outputMatrix).toEqual(expectedOutputMatrix);
+    })
+});
